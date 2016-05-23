33cc81dd1561823a4f7f9ee18a1966f9f96f1f3b
Joao Pedro Sacheti <jpsacheti@gmail.com>
2016-05-23 10:01:51 -0300

    Adições no ticketDao

--- src/main/java/br/edu/fema/sacheti/dao/TicketDao.java
+++ src/main/java/br/edu/fema/sacheti/dao/TicketDao.java
@@ -1,5 +1,7 @@
 package br.edu.fema.sacheti.dao;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
@@ -11,5 +13,17 @@ public class TicketDao {
 	
 	public void cadastrar(Ticket ticket){
 		session.persist(ticket);
+	}
+	
+	public void alterar(Ticket ticket){
+		session.merge(ticket);
+	}
+	
+	public void excluir(Ticket ticket){
+		session.remove(ticket);
+	}
+	
+	public List<Ticket> buscarTodos(){
+		return session.createQuery("select t from Ticket t", Ticket.class).getResultList();
 	}
 }
